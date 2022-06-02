443ec0cbe5b7938718fa149a1cce184c43b11d23
kamal <tnk6195@gmail.com>
2022-06-02 08:57:42 +0530

    week03 day02 cc

---
+++ coding-challenges/week03/day02/program.js
@@ -0,0 +1,19 @@
+//OR logical operator true+false = true
+var a=10
+var b=5
+console.log (a>=10 || b>5)
+
+//OR logical operator false+true = true
+var c=20
+var d=15
+console.log (c>20 || d==15)
+
+//OR logical operator true+true = true
+var e=16
+var f=17
+console.log (e<=16 || f>=17)
+
+//OR logical operator false+false = false
+var g=50
+var h=55
+console.log (g==70 || h>=60)
