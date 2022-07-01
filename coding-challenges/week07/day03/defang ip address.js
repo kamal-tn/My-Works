6449ca9a7bf429c9b27894e386fb6577f8bb8030
kamal <tnk6195@gmail.com>
2022-07-01 19:34:19 +0530

    week07 day03 cc

---
+++ coding-challenges/week07/day03/defang ip address.js
@@ -0,0 +1,3 @@
+var defangIPaddr = function(address) {
+    return address.split(".").join('[.]')}
+   console.log (defangIPaddr ("1.1.1.1"))
---
+++ coding-challenges/week07/day03/sort array.js
@@ -0,0 +1,5 @@
+const array = [20,2,71,65,89,12]
+function compare (a,b){
+    return a-b;
+}
+console.log (array.sort (compare))
