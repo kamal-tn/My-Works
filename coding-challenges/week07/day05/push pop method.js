3ead3b322cd992ff0ee9fea1d6679386bdaa285e
kamal <tnk6195@gmail.com>
2022-07-04 12:24:22 +0530

    week07 day05 cc

---
+++ coding-challenges/week07/day05/push pop method.js
@@ -0,0 +1,4 @@
+const array = [1,2,3,4,5]
+array.pop();
+array.push("hello");
+console.log (array);
---
+++ coding-challenges/week07/day05/typeof.js
@@ -0,0 +1,5 @@
+const array = [1,2,true,4,5,"9",false,10]
+function type(num){
+    console.log (typeof num)
+}
+array.forEach (type)
