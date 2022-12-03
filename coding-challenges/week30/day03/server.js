2bd0ad106986a17c146684eca4a10053698d235d
kamal <tnk6195@gmail.com>
2022-12-03 22:40:03 +0530

    week30 day03 cc

---
+++ coding-challenges/week30/day03/server.js
@@ -0,0 +1,36 @@
+var express = require('express');
+const path = require('path');
+var bodyParser = require('body-parser');
+var multer = require('multer');
+var upload = multer();
+var app = express();
+var serverPort = 8020;
+
+app.set("views", "./views");
+app.set("view engine", "ejs");
+
+app.get('/', function(request, response){
+    console.log("got get on universal route.");
+    response.render('signupform');
+});
+
+
+app.use(bodyParser.urlencoded({extended : true}));
+
+app.use(upload.array());
+
+app.post('/',function(request,response){
+    console.log(request.body);
+    response.send("Recieved Post Request.");
+});
+
+app.listen(serverPort, (error)=>{
+    if(!error)
+    {
+        console.log("Server is started at :  "+serverPort);
+    }
+    else
+    {
+        console.log("Server encountered an error : "+error+", while starting.");
+    }
+});
